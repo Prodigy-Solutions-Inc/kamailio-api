2b9ee1222293362d558ecd09efb484f4ad06411f
prodigytel <fmusick@prodigytel.com>
2023-05-15 17:27:25 -0400

    Added terminating slash as valid route

--- api/registrations.js
+++ api/registrations.js
@@ -55,6 +55,37 @@ module.exports = [
     },
     {
         method: 'GET',
+        path: '/v1/registration/',
+        handler: async (request, h) => {
+            const logger = request.server.app.logger;
+            const { username, domain } = request.query;
+            logger.info(`Verifying registration status for username: ${username} domain: ${domain}`);
+            const uow = await request.app.getNewUoW();
+            try {
+                const registration = await uow.locationsRepository.getRegistration(username, domain);
+                if (registration && typeof registration === 'object' && registration !== null) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (err) {
+                logger.error(err);
+                logger.error('Error fetching registration status');
+                return Boom.badImplementation('Error fetching registration status');
+            }
+        },
+        options: {
+            auth: false,
+            validate: {
+                query: {
+                    username: Joi.string().optional(),
+                    domain: Joi.string().optional()
+                }
+            }
+        }
+    },
+    {
+        method: 'GET',
         path: '/v1/registration/expires',
         handler: async (request, h) => {
             const logger = request.server.app.logger;
